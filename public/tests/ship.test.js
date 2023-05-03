5e6417ada03e7eab3c48e7c246198bb5b5fbdce4
Ondra Svec <ondrasvecveslo@gmail.com>
2023-05-03 11:04:29 +0200

    Move test files to a designated folder

--- public/tests/ship.test.js
+++ public/tests/ship.test.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { describe, test, expect, beforeEach } from "vitest";
-import Ship from "./ship";
+import Ship from "../modules/ship";
 
 describe("Ship factory objects", () => {
   let testShip;
