4931dac38ce9d20e96000e09cdd1db7e49e3b2a4
Ondra Svec <ondrasvecveslo@gmail.com>
2023-05-05 13:22:08 +0200

    Add GameController startGame function

--- public/modules/gameController.js
+++ public/modules/gameController.js
@@ -1,13 +1,81 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
 import Ship from "./ship";
 import Gameboard from "./gameboard";
 import Player from "./player";
 import ComputerPlayer from "./computerPlayer";
 
-const GameController = () => {
-  const humanBoard = Gameboard();
-  const compBoard = Gameboard();
-  const humanPlayer = Player(compBoard);
-  const compPlayer = ComputerPlayer(humanBoard);
+const GameController = (() => {
+  let humanBoard;
+  let compBoard;
+  let humanPlayer;
+  let compPlayer;
+
+  const startGame = () => {
+    humanBoard = Gameboard();
+    compBoard = Gameboard();
+    humanPlayer = Player(compBoard);
+    compPlayer = ComputerPlayer(humanBoard);
+
+    const humanCarrier = Ship("Carrier");
+    const compCarrier = Ship("Carrier");
+    const humanBattleship = Ship("Battleship");
+    const compBattleship = Ship("Battleship");
+    const humanCruiser = Ship("Cruiser");
+    const compCruiser = Ship("Cruiser");
+    const humanSubmarine = Ship("Submarine");
+    const compSubmarine = Ship("Submarine");
+    const humanDestroyer = Ship("Destroyer");
+    const compDestroyer = Ship("Destroyer");
+
+    const availablePositionsHumanBrd = [];
+    for (let i = 0; i < 10; i += 1) {
+      for (let j = 0; j < 10; j += 1) {
+        availablePositionsHumanBrd.push([i, j]);
+      }
+    }
+
+    const availablePositionsCompBrd = [];
+    for (let i = 0; i < 10; i += 1) {
+      for (let j = 0; j < 10; j += 1) {
+        availablePositionsCompBrd.push([i, j]);
+      }
+    }
+
+    const findHorizontalPlacement = (board, shipLength) => {
+      let placementArr = [];
+
+      const startPosition = board.filter(
+        (coord) => coord[1] <= 9 - (shipLength - 1)
+      );
+      const randomPos = Math.floor(Math.random() * startPosition.length);
+
+      for (let i = 0; i < 5; i += 1) {
+        placementArr.push(startPosition[randomPos]);
+      }
+      placementArr = placementArr.map(
+        (item, index) => (item = [item[0], item[1] + index])
+      );
+      return placementArr;
+    };
+
+    const findVerticalPlacement = (board, shipLength) => {
+      let placementArr = [];
+
+      const startPosition = board.filter(
+        (coord) => coord[0] <= 9 - (shipLength - 1)
+      );
+      const randomPos = Math.floor(Math.random() * startPosition.length);
+
+      for (let i = 0; i < shipLength; i += 1) {
+        placementArr.push(startPosition[randomPos]);
+      }
+      placementArr = placementArr.map(
+        (item, index) => (item = [item[0] + index, item[1]])
+      );
+      return placementArr;
+    };
+  };
 
   let activePlayer = humanPlayer;
 
@@ -18,7 +86,7 @@ const GameController = () => {
 
   const getActivePlayer = () => activePlayer;
 
-  return { switchPlayers, getActivePlayer };
-};
+  return { startGame, switchPlayers, getActivePlayer };
+})();
 
 export default GameController;
